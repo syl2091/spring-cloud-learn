b08fe682ba76fee4838f1f612a40e5ed9fd19c3a
onthroad <164378335@qq.com>
2022-08-11 13:44:58 +0800

    Spring Cloud Consul：服务治理与配置中心

---
+++ akka-moudle/README.md
@@ -0,0 +1,1 @@
+# akka构建httpapi
---
+++ akka-moudle/akka-http/src/main/java/com/lege/akkahttp/UserActor.java
@@ -0,0 +1,41 @@
+package com.lege.akkahttp;
+
+import akka.actor.AbstractActor;
+import akka.actor.Props;
+import akka.japi.pf.FI;
+import com.lege.akkahttp.UserMessages.ActionPerformed;
+import com.lege.akkahttp.UserMessages.CreateUserMessage;
+import com.lege.akkahttp.UserMessages.GetUserMessage;
+
+
+class UserActor extends AbstractActor {
+
+  private UserService userService = new UserService();
+
+  static Props props() {
+    return Props.create(UserActor.class);
+  }
+
+  @Override
+  public Receive createReceive() {
+    return receiveBuilder()
+            .match(CreateUserMessage.class, handleCreateUser())
+            .match(GetUserMessage.class, handleGetUser())
+            .build();
+  }
+
+  private FI.UnitApply<CreateUserMessage> handleCreateUser() {
+    return createUserMessageMessage -> {
+      userService.createUser(createUserMessageMessage.getUser());
+      sender().tell(new ActionPerformed(String.format("User %s created.", createUserMessageMessage.getUser()
+              .getName())), getSelf());
+    };
+  }
+
+  private FI.UnitApply<GetUserMessage> handleGetUser() {
+    return getUserMessageMessage -> {
+      sender().tell(userService.getUser(getUserMessageMessage.getUserId()), getSelf());
+    };
+  }
+
+}
